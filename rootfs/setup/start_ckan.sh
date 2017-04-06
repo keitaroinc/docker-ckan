db0b33437d1d85f2f46529aac0f37fe7b71282c1
Marko Bocevski <marko.bocevski@keitaro.com>
2017-04-06 11:25:21 +0200

    Change docker layout and setup install for CKAN 2.6.2 with password protect option

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -2,7 +2,21 @@
 python prerun.py
 if [ $? -eq 0 ]
 then
-  gunicorn --log-file=- -k gevent -w 4 --paste production.ini
+  if [ "$PASSWORD_PROTECT" = true ]
+  then
+    if [ "$HTPASSWD_USER" ] || [ "$HTPASSWD_PASSWORD" ]
+    then
+      cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
+      htpasswd -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
+      nginx
+      gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+    else
+      echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
+      exit 1
+    fi
+  else
+    gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
+  fi
 else
   echo "[prerun] failed...not starting CKAN."
 fi
