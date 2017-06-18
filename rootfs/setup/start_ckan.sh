256d0e7fcbad63aeac688cb0760afc716fd7e77b
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2017-06-18 20:58:12 +0200

    Remove redirect

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -2,14 +2,24 @@
 python prerun.py
 if [ $? -eq 0 ]
 then
-  if [ "$HTTPS_REDIRECT" -eq "1" ]
+  if [ "$PASSWORD_PROTECT" = true ]
   then
-    cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
-    nginx
-    gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+    if [ "$HTPASSWD_USER" ] || [ "$HTPASSWD_PASSWORD" ]
+    then
+      cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
+      htpasswd -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
+      nginx
+      supervisord --configuration /etc/supervisord.conf &
+      gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+    else
+      echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
+      exit 1
+    fi
   else
+    supervisord --configuration /etc/supervisord.conf &
     gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
   fi
 else
   echo "[prerun] failed...not starting CKAN."
 fi
+
