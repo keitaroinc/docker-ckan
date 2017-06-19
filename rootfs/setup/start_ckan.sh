f16027e550f0cb9326380f2f04c9be6c03ac2757
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2017-06-19 09:19:21 +0200

    Add redirect

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -2,19 +2,12 @@
 python prerun.py
 if [ $? -eq 0 ]
 then
-  if [ "$PASSWORD_PROTECT" = true ]
+  if [ "$HTTPS_REDIRECT" -eq "1" ]
   then
-    if [ "$HTPASSWD_USER" ] || [ "$HTPASSWD_PASSWORD" ]
-    then
       cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
-      htpasswd -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
       nginx
       supervisord --configuration /etc/supervisord.conf &
       gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
-    else
-      echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
-      exit 1
-    fi
   else
     supervisord --configuration /etc/supervisord.conf &
     gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
@@ -22,4 +15,3 @@ then
 else
   echo "[prerun] failed...not starting CKAN."
 fi
-
