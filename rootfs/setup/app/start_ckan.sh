5b2f5cef9a5222a7d712311a980b01f6d7196d98
Marko Bocevski <marko.bocevski@keitaro.com>
2020-06-11 10:30:11 +0200

    Use gevent early monkey patch due to upstream issues

--- rootfs/setup/app/start_ckan.sh
+++ rootfs/setup/app/start_ckan.sh
@@ -16,7 +16,7 @@ fi
 python prerun.py
 
 # Set the common uwsgi options
-UWSGI_OPTS="--socket /tmp/uwsgi.sock --uid 92 --gid 92 --http :5000 --master --enable-threads --paste config:/srv/app/production.ini --lazy-apps --gevent 2000 -p 2 -L"
+UWSGI_OPTS="--socket /tmp/uwsgi.sock --uid 92 --gid 92 --http :5000 --master --enable-threads --paste config:/srv/app/production.ini --lazy-apps --gevent 2000 -p 2 -L --gevent-early-monkey-patch"
 
 # Check whether http basic auth password protection is enabled and enable basicauth routing on uwsgi respecfully
 if [ $? -eq 0 ]
