e438bb70f3068cec5a380de12d1d57743c0bb9dd
Marko Bocevski <marko.bocevski@keitaro.com>
2020-09-27 13:00:48 +0200

    Fix ckan config-tool in 2.9 and pin greenlet

--- images/datapusher/setup/datapusher_settings.py
+++ images/datapusher/setup/datapusher_settings.py
@@ -32,6 +32,9 @@ CHUNK_SIZE = int(os.environ.get('DATAPUSHER_CHUNK_SIZE', '16384'))
 CHUNK_INSERT_ROWS = int(os.environ.get('DATAPUSHER_CHUNK_INSERT_ROWS', '250'))
 DOWNLOAD_TIMEOUT = int(os.environ.get('DATAPUSHER_DOWNLOAD_TIMEOUT', '30'))
 
+# Verify SSL
+SSL_VERIFY = os.environ.get('DATAPUSHER_SSL_VERIFY', False)
+
 # Rewrite resource URL's when ckan callback url base is used
 REWRITE_RESOURCES = os.environ.get('DATAPUSHER_REWRITE_RESOURCES', False)
 REWRITE_URL = os.environ.get('DATAPUSHER_REWRITE_URL', 'http://ckan:5000/')
