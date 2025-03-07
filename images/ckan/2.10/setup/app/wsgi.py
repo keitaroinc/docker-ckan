06962a44c70918a1d7e88f005970f3122e431762
Marko Bocevski <marko.bocevski@keitaro.com>
2025-03-07 12:03:09 +0100

    Update base alpine image to latest and refactor ckan docker images

--- images/ckan/2.10/setup/app/wsgi.py
+++ images/ckan/2.10/setup/app/wsgi.py
@@ -20,8 +20,10 @@ import os
 from ckan.config.middleware import make_app
 from ckan.cli import CKANConfigLoader
 from logging.config import fileConfig as loggingFileConfig
+
 config_filepath = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), u'production.ini')
+    os.path.dirname(os.path.abspath(__file__)), "production.ini"
+)
 abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 loggingFileConfig(config_filepath)
 config = CKANConfigLoader(config_filepath).get_config()
